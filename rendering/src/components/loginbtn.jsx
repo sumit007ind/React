8e49f68e30ab3fb2e6fdccf05dae7a45f662c4fa
sumit007ind <sharmaraj81808@gmail.com>
2024-09-21 01:48:12 +0530

    ..

---
+++ rendering/src/components/loginbtn.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const loginbtn = () => {
+  return (
+    <button>
+      Login
+    </button>
+  )
+}
+
+export default loginbtn
+
---
+++ rendering/src/components/logoutbtn.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const logoutbtn = () => {
+  return (
+   <button>
+    logout
+   </button>
+  )
+}
+
+export default logoutbtn
+
