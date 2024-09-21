71ad91645561e172767d2107053b4018fb6e9633
sumit007ind <sharmaraj81808@gmail.com>
2024-09-22 01:47:35 +0530

    added new parra

--- EventHandle/src/App.jsx
+++ EventHandle/src/App.jsx
@@ -17,6 +17,7 @@ function App() {
         <input type='text' onChange={handleInputchange}/>
       </form>
       <button className=''>my name is sumit sharma </button>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quidem nesciunt reprehenderit a, quaerat quibusdam illum sunt recusandae sequi culpa id incidunt ipsam libero. Ad exercitationem veniam, necessitatibus minus voluptatum, dolore vero itaque magnam modi veritatis quas, quisquam sit pariatur odio optio vel repellat tempore consectetur eveniet aspernatur aliquid quibusdam dignissimos officia harum? Rerum tempore molestias, unde repudiandae sint sunt ullam placeat rem quaerat. Consectetur dolorem ad accusantium molestias quaerat repudiandae optio, magnam praesentium nesciunt quasi, laudantium quod? Quibusdam quis deserunt expedita et?</p>
     </div>
   //  <div>
   //   <button onClick={handleCLick}>Click me</button>
