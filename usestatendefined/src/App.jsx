ecdaaebc11090db3fb55ca83b29b7cccd82c174a
sumit007ind <sharmaraj81808@gmail.com>
2024-09-24 11:23:32 +0530

    ..

--- usestatendefined/src/App.jsx
+++ usestatendefined/src/App.jsx
@@ -1,20 +1,25 @@
 
 import './App.css'
-import { useCallback } from 'react'
+import { useCallback, useEffect, useState } from 'react'
+function App(){
+  const [count , setCount]= useState(0);
+}
+useEffect(()=>{
+  alert ("i will run on each render")
+})
 
-function App() {
-  useEffect(() => {
-    first
-  
-    return () => {
-      second
-    }
-  }, [third])
+
+  function handleclick(){
+    setCount(count+1);
+
+  }
   
 
   return (
-   <div>Sumit Sharma</div>
+   <div>
+    <button onClick={handleclick}>click me </button>
+   </div>
   )
-}
+
 
 export default App
