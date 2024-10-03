afe701645c2c6ff8337064b6721785fb411d8d3f
sumit007ind <sharmaraj81808@gmail.com>
2024-10-04 02:33:48 +0530

    ...

--- reacthook/src/App.jsx
+++ reacthook/src/App.jsx
@@ -9,13 +9,15 @@ function App() {
     watch,
     formState: { errors },
   } = useForm();
-  
+   function onsubmit(){
+      console.log("submitting the form , data");
+   }
 
   return (
-   <form onSubmit={handleSubmit((data) => console.log (data))}>
+   <form onSubmit={handleSubmit(onsubmit)}>
     <div>
       <label >first name</label>
-      <input {...register("first name")}/>
+      <input {...register("first name" ,{ required: true })}/>
     </div>
     <div>
       <label >middle name </label>
@@ -25,6 +27,8 @@ function App() {
       <label >Last name</label>
       <input {...register("last name")}/>
     </div>
+    <br/>
+    <input type='submit'/>
    </form>
 
   ) 
