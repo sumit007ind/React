5f1665a9886ed355b05428691bee9b8fd09e7d9c
sumit007ind <sharmaraj81808@gmail.com>
2024-10-07 00:47:37 +0530

    ...

--- reactredux/src/main.jsx
+++ reactredux/src/main.jsx
@@ -3,9 +3,11 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { Provider } from 'react-redux'
-import { store } from './store'
+import { store } from './redux/store.js' 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <Provider store={store}>
     <App />
+    </Provider>
   </StrictMode>,
 )
