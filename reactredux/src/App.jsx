c578ca695cd726bc64afea6e84d522f83b460752
sumit007ind <sharmaraj81808@gmail.com>
2024-10-07 21:32:38 +0530

    ...

--- reactredux/src/App.jsx
+++ reactredux/src/App.jsx
@@ -1,12 +1,28 @@
 
 import { useDebugValue } from 'react'
 import './App.css'
+import { useDispatch, useSelector } from 'react-redux'
 
 function App() {
- 
+  const count = useSelector({state} => state.counter.value);
+  const dispatch = useDispatchs();
+
+
+
+ function handleIncrementCLick(){
+
+ }
+ function handleDecrementClick(){
+
+ }
+
 
   return (
-    <div> Sumit Sharma</div>
+    <div className='container'>
+  <button onClick={handleIncrementCLick}>+</button>
+  <h2>Click Me</h2>
+  <button onClick={handleDecrementClick}>-</button>
+    </div>
   )
 }
 
