e56aa1113484b2cdc97ee30cb9483000f6c7e12a
sumit007ind <sharmaraj81808@gmail.com>
2024-10-08 01:20:46 +0530

    added new value or reference

--- useref/src/App.jsx
+++ useref/src/App.jsx
@@ -1,14 +1,25 @@
 
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
   const [count ,setCount] =useState(0);
+
+  function handleIncrement(){
+    val =  val+1;
+    console.log("value of val",  val);
+    setCount(count +1);
+  }
+
+useEffect(() => {
+ console.log("main firse render ho rha hu ")
+})
+
   
 
   return (
   <div>
-    <button>Increment</button>
+    <button onClick={handleIncrement}>Increment</button>
     <br/>
     <br/>
     <button>
