ca33c05bc9febd86df7d5983e960411090a9cf4c
sumit007ind <sharmaraj81808@gmail.com>
2024-10-10 01:40:50 +0530

    using react.memo

--- usecallback/src/component/ChildComponent.jsx
+++ usecallback/src/component/ChildComponent.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 
-const ChildComponent = (props) => {
-    console.log("Child Component go re- rendered:");
-  return (
-    <div>
-      {props.buttonName}
-    </div>
-  )
-}
+const ChildComponent = React.memo(
+    (props) => {
+        console.log("Child Component go re- rendered")
+      return (
+        <div>
+          {props.buttonName}
+        </div>
+      )
+    }
+);
 
 export default ChildComponent
 
