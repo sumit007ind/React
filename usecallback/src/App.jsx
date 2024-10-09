f1fe16a691d7f48ef951da388b90a921e6d22fba
sumit007ind <sharmaraj81808@gmail.com>
2024-10-10 01:26:09 +0530

    importing the callbacks

--- usecallback/src/App.jsx
+++ usecallback/src/App.jsx
@@ -1,11 +1,23 @@
-
+import {useState} from 'react'
 import './App.css'
 
 function App() {
+  const[count , setCount]= useState(0);
+
+  function handleClick(){
+    setCount(count+2);
+  }
  
 
   return (
-    <div>Usecallback</div>
+    <div>
+      <div>
+        count:{count}
+      </div>
+      <button onClick = {handleClick}>
+       Increment
+      </button>
+    </div>
   )
 }
 
