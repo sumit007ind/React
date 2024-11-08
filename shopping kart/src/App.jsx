3e3f476396b6d56723acfa3908aae26beeb2c75e
sumit007ind <sharmaraj81808@gmail.com>
2024-11-08 13:40:12 +0530

    ...

--- shopping kart/src/App.jsx
+++ shopping kart/src/App.jsx
@@ -1,6 +1,4 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 import ShoppingKart from './components/ShoppingKart'
 import Profile from './components/Profile'
@@ -13,7 +11,7 @@ function App() {
 
   return (
   
-    
+    <ShoppingKart/>
     <Profile/>
   )
 }
